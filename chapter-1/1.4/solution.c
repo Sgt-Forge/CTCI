ed260d609091907012b5b86ff9fe6434c3d3b800
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2020-04-25 15:44:54 -0400

    1.4

--- chapter-1/1.4/solution.c
+++ chapter-1/1.4/solution.c
@@ -12,11 +12,33 @@ void buildHashMap(char* phrase, int* map){
 int isPalindrome(char* phrase){
     int freqs[26] = {0};
     buildHashMap(phrase, freqs);
+    int numOddCounts = 0;
     for (int i = 0; i < 26; ++i){
-        printf("[DBG]:\tIndex [%d]:\t%d\n", i, freqs[i]);
+        if (freqs[i] %2 != 0){
+            ++numOddCounts;
+        }
+    }
+    if (strlen(phrase) %2 == 0){
+        return !numOddCounts;
+    } else {
+       if (numOddCounts == 1){
+           return 1;
+       } 
+       return 0;
     }
     
     return 0;
+}
+
+void removeSpaces(char* str){
+    int count = 0;
+    int total_len = strlen(str);
+    for(int i = 0; i < total_len; ++i){
+        if (str[i] != ' '){
+            str[count++] = str[i];
+        }
+    }
+    str[count] = '\0';
 }
 
 int main(int argc, char* argv[]){
@@ -25,7 +47,13 @@ int main(int argc, char* argv[]){
         return -1;
     }
     char* phrase = argv[1];
-    int ret_val = isPalindrome(phrase);
+    removeSpaces(phrase);
+
+    if(isPalindrome(phrase)){
+        printf("[RESULT]:\tYes!  Your word is a permutation of a palindrome!\n");
+    } else {
+        printf("[RESULT]:\tNo!  Your word is NOT a permutation of a palindrome!\n");
+    }
 
     return 0;
 }
