0ca4f0e16faca35af40b14fcddcd67403dd5bfa0
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2020-05-23 11:34:40 -0400

    8.12

--- chapter-8/8.12/solution.cpp
+++ chapter-8/8.12/solution.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <vector>
 
-void printBoard(std::vector< std::vector<int> > );
-
-void invalidRow(std::vector< std::vector<int> >&, int);
-void invalidCol(std::vector< std::vector<int> >&, int);
-void invalidDiag(std::vector< std::vector<int> >&, int, int);
-
-void queens();
-void queensHelper();
-
-
 void printBoard(std::vector< std::vector<int> > board){
     std::cout << "========================================\n" ;
     for(auto row = board.begin(); row != board.end(); ++row){
         for(auto col = (*row).begin(); col != (*row).end(); ++col){
-            std::cout << *col << ", " ;
+            if (*col != -1){
+                std::cout << "|  " << *col << "\t" ;
+            } else {
+                std::cout << "| " << *col << "\t" ;
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+void printSolution(std::vector< std::vector<int> > board){
+    std::cout << "-----------------------------------------------------------------\n";
+    std::cout << "|                      NEW SOLUTION                             |\n";
+    std::cout << "-----------------------------------------------------------------\n";
+    for(auto row = board.begin(); row != board.end(); ++row){
+        for(auto col = (*row).begin(); col != (*row).end(); ++col){
+            if (*col != -1){
+                std::cout << "|  " << *col << "\t" ;
+            } else {
+                std::cout << "| " << *col << "\t" ;
+            }
         }
         std::cout << std::endl;
     }
@@ -23,13 +32,17 @@ void printBoard(std::vector< std::vector<int> > board){
 
 void invalidRow(std::vector < std::vector<int> >& board, int row){
     for (int col = 0; col < board[row].size(); ++col){
-        board[row][col] = -1;
+        if( board[row][col] != 9){
+            board[row][col] = -1;
+        }
     }
 }
 
 void invalidCol(std::vector < std::vector<int> >& board, int col){
     for (auto row = board.begin(); row != board.end() ; ++row){
-        (*row)[col] = -1;
+        if( (*row)[col] != 9){
+            (*row)[col] = -1;
+        }
     }
 }
 
@@ -40,27 +53,72 @@ void invalidDiag(std::vector< std::vector<int> >& board, int row, int col){
     int tRow = row-1;
     int tCol = col-1;
     while (tRow >=0 && tCol >= 0){
-        board[tRow--][tCol--] = -1;
+        if( board[tRow][tCol] != 9){
+            board[tRow][tCol] = -1;
+        }
+        --tRow;
+        --tCol;
     }
     // Going up and right 
     tRow = row-1;
     tCol = col+1;
     while( tRow >=0 && tCol < MAX_COL){
-        board[tRow--][tCol++] = -1;
+        if( board[tRow][tCol] != 9){
+            board[tRow][tCol] = -1;
+        }
+        --tRow;
+        ++tCol;
     }
 
     // Going down and right 
     tRow = row+1;
     tCol = col+1;
     while( tRow < MAX_ROW && tCol < MAX_COL){
-        board[tRow++][tCol++] = -1;
+        if( board[tRow][tCol] != 9){
+            board[tRow][tCol] = -1;
+        }
+        ++tRow;
+        ++tCol;
     }
 
     // Going down and left
     tRow = row+1;
     tCol = col-1;
     while( tRow < MAX_ROW && tCol >= 0){
-        board[tRow++][tCol--] = -1;
+        if( board[tRow][tCol] != 9){
+            board[tRow][tCol] = -1;
+        }
+        ++tRow;
+        --tCol;
+    }
+}
+
+void queensHelper(std::vector< std::vector<int> > board, int row, int col){
+    for(int newRow = row; newRow < board.size(); ++newRow){
+        auto newBoard = board;
+        if(newBoard[newRow][col] == 0){ // Found valid position, emplace new queen and recurse
+            newBoard[newRow][col] = 9;
+            invalidRow(newBoard, newRow);
+            invalidCol(newBoard, col);
+            invalidDiag(newBoard, newRow, col);
+            if(col == board[row].size()-1){ // In the last col, assume we can print the board
+                printSolution(newBoard);
+            } else {
+                queensHelper(newBoard, 0, col+1);
+            }
+        }
+    }
+}
+
+void queens(std::vector< std::vector<int> > board){
+    // Iterate through the rows to explore placing the FIRST queen in different rows
+    for(int row = 0; row != board.size(); ++row){
+        auto newBoard = board; // This is bad.  We should avoid copy constructors
+        newBoard[row][0] = 9;
+        invalidRow(newBoard, row);
+        invalidCol(newBoard, 0);
+        invalidDiag(newBoard, row, 0);
+        queensHelper(newBoard, 0, 1);
     }
 }
 
@@ -88,12 +146,15 @@ int main(){
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0}
     };
+    /*
     invalidDiag(board8, 0, 0);
     printBoard(board8);
     invalidDiag(board8, 0, 7);
     printBoard(board8);
     invalidDiag(board8, 4, 2);
     printBoard(board8);
+    */
+    queens(board5);
 
     return 0;
 }
